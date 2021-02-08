1773e4dd3f7de5220c828edaac7dd45de97c922c
Andy Sanchez <sanchezzandy206@gmail.com>
2021-02-08 22:43:33 +0000

    Updated the code from any mispelled words

--- Dictionary2.java
+++ Dictionary2.java
@@ -8,14 +8,14 @@ public class Dictionary2 extends Book2
   public Dictionary2 (int numPages,int numDefinitions)
   {
     super (numPages);
-    defenitions = numDefinitions;
+    definitions = numDefinitions;
   }
   //--------------------------------------
   // Prints a message using both local and inherited values
   //--------------------------------------
   public void definitionMessage()
   {
-    System.out.println ("Number of definitions: " + defenitions);
-    System.out.println ("Defenitions per page: " + defenitions/pages);
+    System.out.println ("Number of definitions: " + definitions);
+    System.out.println ("Defenitions per page: " + definitions/pages);
   }
 }
--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 class Main 
 {
-  //----------------------------------------
+  //-----------------------------------------------------------
   // Instantiates a derived class and invokes its inherited and
   // local methods
-  //----------------------------------------
+  //------------------------------------------------------------
   public static void main(String[] args) 
   {
     Dictionary2 webster = new Dictionary2 (1500,52500);
     webster.pageMessage();
-    webster.defentionMessage();
+    webster.definitionMessage();
   }
 }

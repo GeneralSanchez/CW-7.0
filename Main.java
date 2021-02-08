4a8ce13813decbcbc5f84ccc044ee542b020167e
Andy Sanchez <sanchezzandy206@gmail.com>
2021-02-08 22:44:19 +0000

    Added name

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ class Main
   //------------------------------------------------------------
   public static void main(String[] args) 
   {
+    System.out.println ("Andy Sanchez, CW 7.0");
     Dictionary2 webster = new Dictionary2 (1500,52500);
     webster.pageMessage();
     webster.definitionMessage();
